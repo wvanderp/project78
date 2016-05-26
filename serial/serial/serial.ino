7f56acd8ac67638f43d0bef115ae7c4ec1dcd526
wouter van der plas <wouter van der plas>
2016-05-26 21:34:41 +0200

    a serial code

---
+++ serial/serial/serial.ino
@@ -0,0 +1,13 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); // RX, 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
