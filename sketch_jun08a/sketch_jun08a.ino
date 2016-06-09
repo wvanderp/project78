498c23a9cbe868afd6305d69698142c5f63fec30
wouter van der plas <wouter van der plas>
2016-06-09 09:51:43 +0200

    some small eddits

--- sketch_jun08a/sketch_jun08a.ino
+++ sketch_jun08a/sketch_jun08a.ino
@@ -3,47 +3,40 @@ int motorLeft = 10;
 int motorRight = 11;
 Servo motorL;
 Servo motorR;
+
 void setup() {
   // put your setup code here, to run once:
-//pinMode(motorLeft,OUTPUT);
-//pinMode(motorRight,OUTPUT);
-motorL.attach(motorLeft);
-motorR.attach(motorRight);
-forward();
+  //pinMode(motorLeft,OUTPUT);
+  //pinMode(motorRight,OUTPUT);
+  motorL.attach(motorLeft);
+  motorR.attach(motorRight);
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
   forward();
 }
+
 void motorLF()
 {
-  //analogWrite(motorLeft,11.5);
   motorL.write(180);
 }
 void motorLR()
 {
-  //analogWrite(motorLeft,21.7);
   motorL.write(0);
 }
 void motorRF()
 {
-  //analogWrite(motorRight,11.5);
-  motorL.write(0);
+  motorR.write(0);
 }
 void motorRR()
 {
-  //analogWrite(motorRight,21.7);
-  motorL.write(180);
+  motorR.write(180);
 }
 void motorIdle()
 {
-  //analogWrite(motorLeft,0);
-  //analogWrite(motorRight,0);
   motorL.write(90);
-  motorL.write(90);
-  
+  motorR.write(90);
 }
 void forward()
 {
