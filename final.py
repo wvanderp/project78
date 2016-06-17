f4d5cca45075a1578b76a4280c7ef3fd4e702f31
wouter van der plas <wouter van der plas>
2016-06-17 15:22:06 +0200

    made some combos

--- final.py
+++ final.py
@@ -1,107 +1,103 @@
-print('start ')
-
 from picamera import PiCamera
 from inspect import getmembers
 from time import sleep
 import serial
 import math
-
-#print('it works ')
-import pprint
-
 import zbar
-#print('zbar ok ')
-
 from PIL import Image
-#print('it works0 ')
 
 ser = serial.Serial(
-	port = "/dev/ttyAMA0",
-	baudrate = 4800,
-	parity = serial.PARITY_NONE,
-	stopbits = serial.STOPBITS_ONE,
-	bytesize = serial.EIGHTBITS,
-	timeout = 1 
+    port="/dev/ttyAMA0",
+    baudrate=4800,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
 )
 
+# qr code vars
+##################
 camera = PiCamera()
 
 scanner = zbar.ImageScanner()
 scanner.parse_config('enable')
 
-
 sizeQrCode = 22.0
 # in cm de diagonaal van de qr
 
 qrCodeDiag = 675.0
-#the diagonal of the qr in px
+# the diagonal of the qr in px
 distance = 50
-#in cm in real life
+# in cm in real life
 
-distfac = (distance/qrCodeDiag)
-#in cm/px de size moet ook in cm zijn
+distfac = (distance / qrCodeDiag)
+# in cm/px de size moet ook in cm zijn
 
-print distfac
+# lidar vars
+#############################
+array = [0] * 180
+
+# qr code data
+##############
+
+qrData = ""
+
 
 def scan():
- 	camera.capture('/home/pi/Desktop/image.jpg')
+    camera.capture('/home/pi/Desktop/image.jpg')
 
-	pil = Image.open('/home/pi/Desktop/image.jpg').convert('L')
-	width, height = pil.size
+    pil = Image.open('/home/pi/Desktop/image.jpg').convert('L')
+    width, height = pil.size
 
-	raw = pil.tostring()
-#	print('it works1 ')
-	image = zbar.Image(width, height, 'Y800', raw)
+    raw = pil.tostring()
+    image = zbar.Image(width, height, 'Y800', raw)
 
-	scanner.scan(image)
-#	print('it works2 ')
+    scanner.scan(image)
 
-	for symbol in image:
-	    # do something useful with results
-	    print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
-            #pprint.pprint(getmembers(symbol))
-            x1 = (symbol.location[0][0]) 
-            x2 = (symbol.location[2][0])
-            y1 = (symbol.location[0][1])
-            y2 = (symbol.location[2][1])
+    for symbol in image:
+        # do something useful with results
+        print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
+        # pprint.pprint(getmembers(symbol))
+        x1 = (symbol.location[0][0])
+        x2 = (symbol.location[2][0])
+        y1 = (symbol.location[0][1])
+        y2 = (symbol.location[2][1])
 
-            qrCenter = ((x1+x2)/2.0)
-            imgCenter = width/2.0
-            centerDiff = imgCenter - qrCenter
-            
-            qrdiag = math.sqrt(math.pow(x2-x1,2)+math.pow(y2-y1,2))
-            
-            scalefac = (10/qrdiag)
-            #scale factor for the backplain
+        qrCenter = ((x1 + x2) / 2.0)
+        imgCenter = width / 2.0
+        centerDiff = imgCenter - qrCenter
 
-            backplain = centerDiff * scalefac
-	    
-            imageDistance = qrdiag*distfac
+        qrdiag = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
 
-            print imageDistance
+        scalefac = (10 / qrdiag)
+        # scale factor for the backplain
 
-            deg = math.degrees(math.atan(backplain/imageDistance))
-            print qrdiag
-            print deg
+        backplain = centerDiff * scalefac
 
+        imageDistance = qrdiag * distfac
 
-            
-#	print('it works3 ')
-	return;
+        angle = math.degrees(math.atan(backplain / imageDistance))
+
+        return symbol.data, angle
+    return
+
 
 while True:
-    ser.write("45&10")
-    scan()
+    # qr code reading
+    angle, data = scan()
 
+    if qrData == data:
+        # sending qr data over serial
+        ser.write(data)
+
+    # reading serial connection
     x = ser.readline()
     if x != "":
         print x
         deg, dist = x.split(":", 2)
         dist = int(dist.rstrip())
-        deg = int(deg)	
-        array[deg-1] = dist
+        deg = int(deg)
+        array[deg - 1] = dist
         print str(deg) + " en " + str(dist)
- 
 
     sleep(1)
-		
